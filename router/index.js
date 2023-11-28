2f2554c7bb0cf197d617733ec759a579867bfff9
Alexis Franco <ronaldofranco196@gmail.com>
2023-11-28 10:20:46 -0300

    carpetas controller, middleware

--- router/index.js
+++ router/index.js
@@ -2,8 +2,5 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
-});
 
 export default router
