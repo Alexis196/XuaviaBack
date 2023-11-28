26d88f1b3671365f8cefe57f1b595c6068f6120b
Alexis Franco <ronaldofranco196@gmail.com>
2023-11-28 10:07:40 -0300

    add rama

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 const app = express()
 
-
+app.get('/', (req, res) => {
+    console.log('hola')
+})
 const PORT = 8080
 const server = app.listen( PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
 server.on =  (error) => console.log(`Error en el servidor ${error.message}`)
