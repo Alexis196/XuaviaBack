92c81c5f920aeb4b1ac43d697b62da6fa8d8e208
Alexis Franco <ronaldofranco196@gmail.com>
2023-11-28 10:17:31 -0300

    rutas e indexRoute

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import indexRouter from './router/index.js'
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded)
+app.use(indexRouter)
 
 const PORT = 8080
 const server = app.listen( PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
