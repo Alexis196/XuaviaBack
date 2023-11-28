a3e5fc56df8d38887a89171eb0f91cee2ba246ab
Alexis Franco <ronaldofranco196@gmail.com>
2023-11-28 10:11:04 -0300

    fixed

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 import express from 'express'
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log('hola')
-})
+
 const PORT = 8080
 const server = app.listen( PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
 server.on =  (error) => console.log(`Error en el servidor ${error.message}`)
