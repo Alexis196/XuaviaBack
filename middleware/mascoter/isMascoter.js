6d86568fe232710946bd2bd00354255f9b95e725
Alexis Franco <108986498+Alexis196@users.noreply.github.com>
2023-12-07 09:17:37 -0300

    middleware isMascoter corregido

--- middleware/mascoter/isMascoter.js
+++ middleware/mascoter/isMascoter.js
@@ -1,20 +1,38 @@
-import express from 'express';
+import mongoose from 'mongoose';
 import User from '../../models/User.js';
+import Rol from '../../models/Rol.js';
 
 const isMascoter = async (req, res, next) => {
-    try {
-        const user = await User.findOne({ _id: req.user.id, is_mascoter: true });
-        if (!user) {
-            return res.status(403).json({
-                success: false,
-                message: 'No eres un mascoter.',
-            });
-        }
-        next();
-    } catch (err) {
-        console.error(`Ocurrió un error: ${err.message}`);
-        next(err);
+  try {
+    console.log('Usuario actual:', req.user);
+
+    const mascoterRole = await Rol.findOne({ rol: 'Mascoter' });
+    console.log('mascoter:',mascoterRole)
+
+    if (!mascoterRole) {
+      return res.status(500).json({
+        success: false,
+        message: 'Error interno del servidor al buscar el rol Mascoter.',
+      });
     }
+
+    const user = await User.findOne({
+      _id: req.user._id,
+      rol: mascoterRole._id,  // Solo necesitas el ID del rol
+    });
+
+    if (!user || String(user.rol) !== String(mascoterRole._id)) {
+      return res.status(403).json({
+        success: false,
+        message: 'Solo los mascoters pueden publicar servicios.',
+      });
+    }
+
+    next();
+  } catch (err) {
+    console.error(`Ocurrió un error: ${err.message}`);
+    next(err);
+  }
 };
 
 export default isMascoter;
